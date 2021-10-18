2805cbf4bb00dd9cb3fae65d9bcb132eb3570603
FrankLaurora <francesco.laurora9@gmail.com>
2021-10-18 17:08:26 +0200

    Add project, unable to properly run it.

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$paragraph = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro vitae, ut dicta tenetur quos necessitatibus cupiditate, harum voluptatum quia ab quisquam veritatis placeat. Ex accusamus, nobis quis quia eaque ipsam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptatem, similique ex libero maxime incidunt magni odio impedit voluptates laboriosam, inventore iste facere labore illo tempore at! Corporis, quidem illum!';
+
+$_GET['badword'];
+
+$newParagraph = str_replace('badword', '***', $paragraph);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Badwords</title>
+    </head>
+    <body>
+        <h2>Paragrafo originale</h2>
+        <p><?php echo $paragraph; ?></p>
+        <p>Questo paragrafo contiene <?php echo strlen($paragraph); ?> caratteri.</p>
+
+        <h2>Paragrafo censurato</h2>
+        <p><?php echo $newParagraph; ?></p>
+        <p>Il paragrafo censurato contiene <?php echo strlen($newParagraph); ?> caratteri.</p>
+
+    </body>
+</html>
